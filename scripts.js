80323cb40bfaa7007bb49e3f1fd41db87382daa9
AmoInno <amogelangmashishi12@gmail.com>
2024-10-27 16:29:30 +0200

    checks if all inputs are numbers

--- scripts.js
+++ scripts.js
@@ -9,16 +9,23 @@ const denominator = document.getElementById("divider").value;
   
 if (numerator === "" || denominator === "") {
 result.textContent = "Division not performed. Both values are required in inputs. Try again";
-return false;
+return;
 }
 
-if (Number(denominator)=== 0) {
+const num1 = parseFloat(numerator);
+const num2 = parseFloat(denominator);
+
+if (num2 === 0) {
 result.innerText = "Division not performed. Invalid number provided. Try again";
 console.error(new Error("Division by 0 is invalid!"));
-return false;
+return;
 }
 
 
+if (isNaN(num1)|| isNaN(num2)){
+  document.body.innerHTML = "Something critical went wrong. Please reload the page";
+  console.error(new Error("All inputs must be numbers"));
+}
 
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
